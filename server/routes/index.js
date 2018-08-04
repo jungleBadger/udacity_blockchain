39b246a9344d7b29d8f8876daaa7557f26614cda
Daniel Abrao <danielpaladar@gmail.com>
2018-08-04 13:06:13 -0300

    Add wallet helper link

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,9 @@
 	"use strict";
 
 
-	module.exports = function (app, blockchain) {
+	module.exports = function (app, blockchain, wallet) {
 		require("./partials/blockHandler")(app, blockchain);
+		require("./partials/walletHandler")(app, wallet);
 	}
 
 }());
